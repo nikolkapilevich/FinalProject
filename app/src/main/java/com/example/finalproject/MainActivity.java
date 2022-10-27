7bd4fe3136e35305c9c0f6f8367a9f9e8e0dca0d
nikol <nikolkapilevich@gmail.com>
2022-10-27 09:10:23 +0300

    login and registration

---
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText username,password;
+    Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+
+    }
+}
